00387720d88d7df939f965ba77a1c77d77f97ce2
Woojae Jun <woojaeizzle@gmail.com>
2017-08-11 19:28:51 +0900

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 //middleware let's us configure how this express app works
 //express.static takes absolute path
@@ -58,6 +59,6 @@ app.get('/about', (req, res) => {
 // });
 
 //bind the app to the port of this machine
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
